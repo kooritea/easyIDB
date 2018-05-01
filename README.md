802b00bbb0bd9c52b6491a8e61615b6e9498cbdc
kooritea <ochatokori@gmail.com>
2018-05-01 19:54:56 +0800

    添加主要文件,完善readme

--- README.md
+++ README.md
@@ -4,25 +4,36 @@
 通过几个简单（？）的函数操作IDB
 
 ## 使用方法
-1、引入
+### 1、引入
 ```js
 import easyIDB from './easy-idb.js'
 ```
 
-2、实例化一个easyIDB对象
-（即打开数据库，没有则创建）
-当前只支持初始化创建一个store
+### 2、实例化一个easyIDB对象
+
+实际上是调用了openDB方法,具体参数参考第三条第一个方法
+
 ```js
 let myIDB = easyIDB(IDBName,newIndex)
 ```
+如果name为空,则返回一个未绑定数据库的easyIDB对象,需要使用openDB打开数据库
+
+### 3、具体方法
+
+#### function openDB(IDBName,newIndex)
+打开数据库，没有则创建
+
+当前只支持初始化创建一个store
+
 参数说明
 
-|   参数名    |         类型          | 必须  |        说明       |
-| :------: | :-------------------: | :--: | :-------------------: |
-| IDBName |      string\object       |  是  |       数据库名      |
-|   newIndex   | object               |  否  |     单个store的索引列表  |
+|  参数名  |        类型     | 必须  |   说明    |
+| :------: | :------------: | :--: | :-------------------: |
+| IDBName |  string\object   |  是  |       数据库名      |
+| newIndex | object          |  否  |     单个store的索引列表  |
 
 当IDBName是字符串时，直接作为数据库名字，打开当前已存在的版本的数据库，不进行数据库更新
+
 当IDBName为对象时，可以指定数据库版本进行强制更新，参考参数如下
 ```js
 ｛
@@ -31,7 +42,8 @@ let myIDB = easyIDB(IDBName,newIndex)
 ｝
 ```
 仅当数据库进行版本更新或者在该用户上第一次创建数据库，会根据newIndex对象进行store的创建
-参考newIndex对象
+
+参考的newIndex对象
 ```js
 {
    name:"xxx",//storeName
@@ -40,7 +52,7 @@ let myIDB = easyIDB(IDBName,newIndex)
             name:"mystore",
             unique:false//是否唯一 默认false
         ｝,
-        
+
             name:"mystore2",
             unique:true//默认值
         ｝,
@@ -57,4 +69,75 @@ let myIDB = easyIDB(IDBName,newIndex)
 ```
 来操作数据库
 
-3、具体方法
+
+---
+
+#### function get(storeName:string,key:string,value)
+
+*这不是TS,只是方便看才这样写,下同
+
+根据给定的storeName和key和value,在指定的store中寻找该对象,并返回一个Promise对象
+
+|  参数名  |        类型     | 必须  |   说明    |
+| :------: | :------------: | :--: | :-------------------: |
+| storeName |  string   |  是  |             |
+| key | string          |  是  |     查找根据的键,既可以是主键也可以是索引  |
+| value | string | 是| 查找值|
+
+```js
+async function(){
+  let data = await myIDB.get('mystore','id','23')
+}
+```
+---
+
+#### function push(storeName:string,data:object)
+
+向指定的store添加一条记录
+
+|  参数名  |        类型     | 必须  |   说明    |
+| :------: | :------------: | :--: | :-------------------: |
+| storeName |  string   |  是  |             |
+| data | object          |  是  |     添加的数据  |
+
+```js
+myIDB.push('mystore',{name:'2233'})
+```
+---
+
+#### function del(storeName:string,key:string,value:string)
+
+在指定的store中删除一条记录
+
+|  参数名  |        类型     | 必须  |   说明    |
+| :------: | :------------: | :--: | :-------------------: |
+| storeName |  string   |  是  |             |
+| key | string          |  是  |     查找根据的键,既可以是主键也可以是索引  |
+| value | string | 是| 查找值|
+
+```js
+async functin(){
+  await myIDB.del('mystore','id','2')
+}
+```
+
+---
+
+#### function edit(storeName:string,key:string,value:string,newObject:object)
+
+更新一条记录
+
+|  参数名  |        类型     | 必须  |   说明    |
+| :------: | :------------: | :--: | :-------------------: |
+| storeName |  string   |  是  |             |
+| key | string          |  是  |     查找根据的键,既可以是主键也可以是索引  |
+| value | string | 是| 查找值|
+| newObject| object | 是 | 更新的键值  |
+
+```js
+async functin(){
+  await myIDB.edit('mystore','id','2',{name:"2233"})
+}
+```
+
+#### function openDB(name:string,newIndex:object)
---
+++ easyIDB.js
@@ -0,0 +1,168 @@
+'use strict'
+
+export default async function(name,newIndex){
+  return {
+    DB:name?(await openDB(name,newIndex)):null,
+    openDB,
+    showList,
+    createObjectStore,
+    push,
+    get,
+    del,
+    edit,
+    lock:false
+  }
+}
+
+//列出当前所有store
+function showList(){
+  return this.DB.objectStoreNames
+}
+
+// newIndex:{  // 创建新的objectStore
+//   name:"xxx",
+//   indexs:[],
+//   option:{
+//     keyPath:'id',
+//     autoIncrement:true
+//   }
+// }
+//创建多个store时需要await
+async function createObjectStore(name,newIndex){//默认id为主键并自增
+  this.DB.close();
+  this.DB = await this.openDB({name:this.DB.name,ver:this.DB.version+1},{name,newIndex})
+}
+
+//
+function get(storeName,key,value){
+  let objectStore = this.DB.transaction(storeName).objectStore(storeName);
+  let data = new Array()
+  if(key){
+    objectStore = objectStore.index(key)
+  }
+  return new Promise(async function(reslove){
+    objectStore.openCursor().onsuccess = function(event){
+      let cursor = event.target.result;
+      if(cursor){
+        if(!key||(key&&cursor.value[key] == value)){
+          data.push(cursor.value)
+        }
+        cursor.continue();
+      }
+      else{
+        reslove(data)
+      }
+    }
+  })
+
+}
+
+
+//添加数据
+//storeName:string
+//data:{}
+function push(storeName,data){
+  this.DB.transaction(storeName, "readwrite").objectStore(storeName).add(data);
+}
+
+//根据索引删除该值的对象
+function del(storeName,key,value){
+  let transaction = this.DB.transaction(storeName, "readwrite");
+  let objectStore = transaction.objectStore(storeName);
+  return new Promise(async function(reslove){
+    if(key ===  objectStore.keyPath){
+        objectStore.delete(parseInt(value));
+    }
+    else{
+      let keypath = objectStore.index(key);
+      let request = keypath.openCursor()
+      request.onsuccess = function(e){
+        let cursor = e.target.result;
+        if(cursor){
+          if(cursor.value[key] === value){
+            objectStore.delete(parseInt(cursor.value[objectStore.keyPath]));
+          }
+          cursor.continue();
+        }
+        else{
+          reslove()
+        }
+      }
+    }
+  })
+}
+
+//根据索引编辑一个对象
+// storeName:string
+// key索引名
+// value 索引值
+// newObject:{}需要更新的数据
+function edit(storeName,key,value,newObject){
+  let transaction = this.DB.transaction(storeName, "readwrite");
+  let objectStore = transaction.objectStore(storeName);
+  let objectStoreRequest
+  return new Promise(async function(reslove){
+    if(key ===  objectStore.keyPath){
+      objectStoreRequest = objectStore.get(parseInt(value));
+
+    }
+    else{
+      objectStoreRequest = objectStore.index(key).get(value);
+    }
+    objectStoreRequest.onsuccess = function(){
+      let Record = objectStoreRequest.result||{}
+      for(let key in newObject){
+        if (typeof Record[key] != 'undefined') {
+           Record[key] = newObject[key];
+        }
+      }
+      reslove(objectStore.put(Record))
+    }
+  })
+}
+
+
+// name有两个传入方式 1:直接传入数据库名字,2:对象{name:'xxx',ver:2}
+// newIndex:{  // 创建新的objectStore
+//   name:"xxx",
+//   indexs:[],
+//   option:{
+//     keyPath:'id',
+//     autoIncrement:true
+//   }
+// }
+//创建
+async function openDB(name,newIndex){//name有两个传入方式 1:直接传入数据库名字,2:对象{name:'xxx',ver:2}
+  return await (new Promise(async function(reslove,reject){
+    let indexedDB = window.indexedDB;
+    if(typeof(name) === 'string'){
+      indexedDB = indexedDB.open(name);
+    }else if(typeof(name) === 'object'){
+      indexedDB = indexedDB.open(name.name,name.ver);
+    }
+    indexedDB.onupgradeneeded = function (event) {
+      let db = event.target.result
+      if(newIndex){
+        try{
+          let objectStore = db.createObjectStore(newIndex.name,{
+            keyPath: newIndex.option.keyPath||'id',
+            autoIncrement: newIndex.option.autoIncrement || true
+          })
+          for(let item in newIndex.indexs){
+            objectStore.createIndex(newIndex.indexs[item].name, newIndex.indexs[item].name, {
+                unique: newIndex.indexs[item].unique||false
+            });
+          }
+        }
+        catch(e){
+        }
+      }
+      setTimeout(function(){
+          reslove(db)
+      },1000)
+    }
+    indexedDB.onsuccess = function (event) {
+      reslove(event.target.result)
+    }
+  }))
+}
